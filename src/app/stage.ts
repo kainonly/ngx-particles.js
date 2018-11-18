ab5ab259adb6b826414a4dfebffe96a508d35d07
kain <zhangtqx@vip.qq.com>
2018-11-18 15:02:09 +0800

    Rebuild This Projects

--- src/app/stage.ts
+++ src/app/stage.ts
@@ -14,14 +14,16 @@ export class Stage {
     return [this.width, this.height];
   }
 
-  drawContext() {
+  drawContext = () => {
     this.ctx.beginPath();
+    this.ctx.translate(105, 0);
     this.ctx.arc(100, 75, (1 + Math.random()) * 10, 0, 2 * Math.PI);
     const color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
     this.ctx.fillStyle = color;
     this.ctx.strokeStyle = color;
     this.ctx.fill();
     this.ctx.stroke();
-  }
+    window.requestAnimationFrame(this.drawContext);
+  };
 
 }
