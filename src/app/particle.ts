e4a3f19ec08d80543f2cf720475949870080ac75
kain <zhangtqx@vip.qq.com>
2018-11-27 22:19:06 +0800

    Rebuild This Projects

--- src/app/particle.ts
+++ src/app/particle.ts
@@ -2,9 +2,6 @@ export class Particle {
   private horizontal: number;
   private coordinate: number;
 
-  private horizontal_max_offset: number;
-  private coordinate_max_offset: number;
-
   constructor(range: number[]) {
     const [max_width, max_height] = range;
     this.horizontal = Math.random() * max_width;
