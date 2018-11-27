cea8e347632db81a2e83f7f697192b8aa5c81854
kain <zhangtqx@vip.qq.com>
2018-11-27 22:57:37 +0800

    Rebuild This Projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import { NgxParticlesComponent } from './ngx-particles/ngx-particles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgxParticlesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ngx-particles/ngx-particles.component.ts
@@ -0,0 +1,45 @@
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {Particle} from './particle';
+
+@Component({
+  selector: 'app-ngx-particles',
+  templateUrl: './ngx-particles.component.html',
+  styleUrls: ['./ngx-particles.component.css']
+})
+export class NgxParticlesComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('stageRef') stageRef: ElementRef;
+
+  private ctx: CanvasRenderingContext2D;
+  private width: number;
+  private height: number;
+  private particles: Particle[] = [];
+
+  ngAfterViewInit() {
+    const canvas = this.stageRef.nativeElement;
+    this.ctx = canvas.getContext('2d');
+    const {clientWidth, clientHeight} = canvas.parentElement;
+    canvas.width = this.width = clientWidth;
+    canvas.height = this.height = clientHeight;
+    this.initParticle();
+    this.drawContext();
+  }
+
+  ngOnDestroy() {
+  }
+
+  private initParticle() {
+    const range = [this.width, this.height];
+    for (let i = 0; i < 150; i++) {
+      this.particles[i] = new Particle(range);
+    }
+  }
+
+  private drawContext = () => {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    for (const x of this.particles) {
+      x.draw(this.ctx);
+      x.update();
+    }
+    window.requestAnimationFrame(this.drawContext);
+  };
+}
