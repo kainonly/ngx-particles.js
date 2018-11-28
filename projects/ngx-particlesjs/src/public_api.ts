1437ece07742cf103822b50228bcdd055a0fbaef
kain <zhangtqx@vip.qq.com>
2018-11-28 21:17:00 +0800

    转移至组件库

--- projects/ngx-particlesjs/src/public_api.ts
+++ projects/ngx-particlesjs/src/public_api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-particlesjs
  */
 
-export * from './lib/ngx-particlesjs.service';
-export * from './lib/ngx-particlesjs.component';
 export * from './lib/ngx-particlesjs.module';
